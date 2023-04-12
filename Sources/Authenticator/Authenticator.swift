1e85397eea4597ec7043251eb8bf4b1bf6bead69
Sebastian Villena <97059974+ruisebas@users.noreply.github.com>
2023-04-12 17:30:24 -0400

    feat(Authenticator): Adding Package setup, Authenticator view and supporting models

---
+++ Sources/Authenticator/Authenticator.swift
@@ -0,0 +1,306 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import Amplify
+import SwiftUI
+
+/// The Authenticator component
+public struct Authenticator<SignInContent: View,
+                            ConfirmSignInWithNewPasswordContent: View,
+                            ConfirmSignInWithMFACodeContent: View,
+                            ConfirmSignInWithCustomChallengeContent: View,
+                            SignUpContent: View,
+                            ConfirmSignUpContent: View,
+                            ResetPasswordContent: View,
+                            ConfirmResetPasswordContent: View,
+                            VerifyUserContent: View,
+                            ConfirmVerifyUserContent: View,
+                            SignedInContent: View,
+                            ErrorContent: View,
+                            Header: View,
+                            Footer: View>: View {
+    @Environment(\.authenticationService) var authenticationService
+    @Environment(\.authenticatorState) var state
+    @State private var currentStep: Step = .loading
+    @State private var previousStep: Step = .loading
+    private var initialStep: AuthenticatorInitialStep
+    private var viewModifiers = ViewModifiers()
+    private var contentStates: NSHashTable<AuthenticatorBaseState> = .weakObjects()
+    private let signInContent: SignInContent
+    private let confirmSignInContentWithMFACodeContent: ConfirmSignInWithMFACodeContent
+    private let confirmSignInContentWithCustomChallengeContent: ConfirmSignInWithCustomChallengeContent
+    private let confirmSignInContentWithNewPasswordContent: ConfirmSignInWithNewPasswordContent
+    private let signUpContent: SignUpContent
+    private let confirmSignUpContent: ConfirmSignUpContent
+    private let resetPasswordContent: ResetPasswordContent
+    private let confirmResetPasswordContent: ConfirmResetPasswordContent
+    private let verifyUserContent: VerifyUserContent
+    private let confirmVerifyUserContent: ConfirmVerifyUserContent
+    private let headerContent: Header
+    private let footerContent: Footer
+    private let errorContentBuilder: (Error) -> ErrorContent
+    private let signedInContentBuilder: (SignedInState) -> SignedInContent
+
+    /// Creates an `Authenticator` component
+    /// - Parameter initialStep: The initial step displayed to unauthorized users. Defaults to `.signIn`
+    /// - Parameter signInContent: The content associated with the `.signIn` step. Defaults to a ``SignInView``.
+    /// - Parameter confirmSignInWithMFACodeContent: The content associated with the `.confirmSignInWithCustomChallenge` step. Defaults to a ``ConfirmSignInWithMFACodeView``.
+    /// - Parameter confirmSignInWithCustomChallengeContent: The content associated with the `.confirmSignInWithMFACode` step. Defaults to a ``ConfirmSignInWithCustomChallengeView``.
+    /// - Parameter confirmSignInWithNewPasswordContent: The content associated with the `.confirmSignInWithNewPassword` step. Defaults to a ``ConfirmSignInWithNewPasswordView``.
+    /// - Parameter signUpContent: The content associated with the `.signUp` step. Defaults to a ``SignUpView``.
+    /// - Parameter confirmSignUpContent: The content associated with the `.confirmSignUp` step. Defaults to a ``ConfirmSignUpView``.
+    /// - Parameter resetPasswordContent: The content associated with the `.resetPassword` step. Defaults to a ``ResetPasswordView``.
+    /// - Parameter confirmResetPasswordContent: The content associated with the `.confirmResetPassword` step. Defaults to a ``ConfirmResetPasswordView``.
+    /// - Parameter verifyUserContent: The content associated with the `.verifyUser` step. Defaults to a ``VerifyUserView``.
+    /// - Parameter confirmVerifyUserContent: The content associated with the `.confirmVerifyUser` step. Defaults to a ``ConfirmVerifyUserView``.
+    /// - Parameter errorContent: The content associated with the `.error` step. Defaults to a ``ErrorView``.
+    /// - Parameter headerContent: A custom header content that is displayed on top of any other Authenticator content. Defaults to a ``EmptyView`.
+    /// - Parameter footerContent: A custom footer content that is displayed below any other Authenticator content. Defaults to a ``EmptyView`.
+    /// - Parameter content: The content associated with the `.signedIn` step, i.e. once the user has successfully authenticated.
+    public init(
+        initialStep: AuthenticatorInitialStep = .signIn,
+        @ViewBuilder signInContent: @escaping (SignInState) -> SignInContent = { state in
+            SignInView(state: state)
+        },
+        @ViewBuilder confirmSignInWithMFACodeContent: @escaping (ConfirmSignInWithCodeState) -> ConfirmSignInWithMFACodeContent = { state in
+            ConfirmSignInWithMFACodeView(state: state)
+        },
+        @ViewBuilder confirmSignInWithCustomChallengeContent: @escaping (ConfirmSignInWithCodeState) -> ConfirmSignInWithCustomChallengeContent = { state in
+            ConfirmSignInWithCustomChallengeView(state: state)
+        },
+        @ViewBuilder confirmSignInWithNewPasswordContent: @escaping (ConfirmSignInWithNewPasswordState) -> ConfirmSignInWithNewPasswordContent = { state in
+            ConfirmSignInWithNewPasswordView(state: state)
+        },
+        @ViewBuilder signUpContent: @escaping (SignUpState) -> SignUpContent = { state in
+            SignUpView(state: state)
+        },
+        @ViewBuilder confirmSignUpContent: @escaping (ConfirmSignUpState) -> ConfirmSignUpContent = { state in
+            ConfirmSignUpView(state: state)
+        },
+        @ViewBuilder resetPasswordContent: @escaping (ResetPasswordState) -> ResetPasswordContent = { state in
+            ResetPasswordView(state: state)
+        },
+        @ViewBuilder confirmResetPasswordContent: @escaping (ConfirmResetPasswordState) -> ConfirmResetPasswordContent = { state in
+            ConfirmResetPasswordView(state: state)
+        },
+        @ViewBuilder verifyUserContent: @escaping (VerifyUserState) -> VerifyUserContent = { state in
+            VerifyUserView(state: state)
+        },
+        @ViewBuilder confirmVerifyUserContent: @escaping (ConfirmVerifyUserState) -> ConfirmVerifyUserContent = { state in
+            ConfirmVerifyUserView(state: state)
+        },
+        @ViewBuilder errorContent: @escaping (Error) -> ErrorContent = { _ in
+            ErrorView()
+        },
+        @ViewBuilder headerContent: () -> Header = { EmptyView() },
+        @ViewBuilder footerContent: () -> Footer = { EmptyView() },
+        @ViewBuilder content: @escaping (SignedInState) -> SignedInContent
+    ) {
+        self.initialStep = initialStep
+        let credentials = Credentials()
+
+        let signInState = SignInState(credentials: credentials)
+        contentStates.add(signInState)
+        self.signInContent = signInContent(signInState)
+
+        let confirmSignInWithMFACodeState = ConfirmSignInWithCodeState(credentials: credentials)
+        contentStates.add(confirmSignInWithMFACodeState)
+        self.confirmSignInContentWithMFACodeContent = confirmSignInWithMFACodeContent(
+            confirmSignInWithMFACodeState
+        )
+
+        let confirmSignInWithCustomChallengeState = ConfirmSignInWithCodeState(credentials: credentials)
+        contentStates.add(confirmSignInWithMFACodeState)
+        self.confirmSignInContentWithCustomChallengeContent = confirmSignInWithCustomChallengeContent(
+            confirmSignInWithCustomChallengeState
+        )
+
+        let confirmSignInWithNewPasswordState = ConfirmSignInWithNewPasswordState(credentials: credentials)
+        contentStates.add(confirmSignInWithNewPasswordState)
+        self.confirmSignInContentWithNewPasswordContent = confirmSignInWithNewPasswordContent(
+            confirmSignInWithNewPasswordState
+        )
+
+        let signUpState = SignUpState(credentials: credentials)
+        contentStates.add(signUpState)
+        self.signUpContent = signUpContent(signUpState)
+
+        let confirmSignUpState = ConfirmSignUpState(credentials: credentials)
+        contentStates.add(confirmSignUpState)
+        self.confirmSignUpContent = confirmSignUpContent(confirmSignUpState)
+
+        let resetPasswordState = ResetPasswordState(credentials: credentials)
+        contentStates.add(resetPasswordState)
+        self.resetPasswordContent = resetPasswordContent(resetPasswordState)
+
+        let confirmResetPasswordState = ConfirmResetPasswordState(credentials: credentials)
+        contentStates.add(confirmResetPasswordState)
+        self.confirmResetPasswordContent = confirmResetPasswordContent(confirmResetPasswordState)
+
+        let verifyUserState = VerifyUserState(credentials: credentials)
+        contentStates.add(verifyUserState)
+        self.verifyUserContent = verifyUserContent(verifyUserState)
+
+        let confirmVerifyUserState = ConfirmVerifyUserState(credentials: credentials)
+        contentStates.add(confirmVerifyUserState)
+        self.confirmVerifyUserContent = confirmVerifyUserContent(confirmVerifyUserState)
+
+        self.headerContent = headerContent()
+        self.footerContent = footerContent()
+        self.errorContentBuilder = errorContent
+        self.signedInContentBuilder = content
+    }
+
+    public var body: some View {
+        VStack {
+            Group {
+                if case .signedIn(let user) = currentStep {
+                    let signedInState = SignedInState(
+                        user: user,
+                        authenticationService: authenticationService
+                    )
+                    signedInContentBuilder(signedInState)
+                        .environmentObject(signedInState)
+                } else {
+                    headerContent
+                    createView(for: currentStep)
+                    footerContent
+                }
+            }
+            .transition(contentTransition)
+        }
+        .animation(viewModifiers.contentAnimation, value: currentStep)
+        .environment(\.authenticatorOptions.hidesSignUpButton, viewModifiers.hidesSignUpButton)
+        .environment(\.authenticatorOptions.contentAnimation, viewModifiers.contentAnimation)
+        .environment(\.authenticatorOptions.contentTransition, viewModifiers.contentTransition)
+        .environment(\.authenticatorOptions.signUpFields, viewModifiers.signUpFields)
+        .task {
+            state.authenticationService = authenticationService
+            setUpContentStates(contentStates)
+            await state.reloadState(initialStep: initialStep)
+        }
+        .onChange(of: contentStates) { contentStates in
+            setUpContentStates(contentStates)
+        }
+        .onChange(of: initialStep) { initialStep in
+            Task {
+                await state.reloadState(initialStep: initialStep)
+            }
+        }
+        .onReceive(state.$step) {
+            self.previousStep = self.currentStep
+            self.currentStep = $0
+        }
+    }
+
+    /// Hides the Sign Up Button that is displayed in the default ``SignInView``.
+    /// - Parameter hidesSignUpButton: Whether to hide the Sign Up button. Defaults to true
+    public func hidesSignUpButton(_ hidesSignUpButton: Bool = true) -> Self {
+        var view = self
+        view.viewModifiers.hidesSignUpButton = hidesSignUpButton
+        return view
+    }
+
+    /// Sets the animation used to transition between steps.
+    /// - Parameter animation: The animation that will be used to apply UI changes when the Authenticator's current step changes.
+    public func contentAnimation(_ animation: Animation) -> Self {
+        var view = self
+        view.viewModifiers.contentAnimation = animation
+        return view
+    }
+
+    /// Sets the transition used to transition between steps.
+    /// - Parameter transition: The transition that will be used to apply UI changes when the Authenticator's current step changes.
+    public func contentTransition(_ transition: AnyTransition) -> Self {
+        var view = self
+        view.viewModifiers.contentTransition = transition
+        return view
+    }
+
+    /// Sets the Sign Up fields that will be displayed in the `.signUp` step.
+    /// - Parameter signUpFields: An array of Sign Up fields that will be displayed when signing up. The order of the array is mantained when displaying the fields.
+    public func signUpFields(_ signUpFields: [SignUpField]) -> Self {
+        var view = self
+        view.viewModifiers.signUpFields = signUpFields
+        return view
+    }
+
+    /// Sets a custom error mapping function for the `AuthError`s that are displayed
+    /// - Parameter errorTransform: A closure that takes an `AuthError` and returns a ``AuthenticatorError`` that will be displayed.
+    public func errorMap(_ errorTransform: @escaping (AuthError) -> AuthenticatorError) -> Self {
+        for contentState in contentStates.allObjects {
+            contentState.errorTransform = errorTransform
+        }
+        return self
+    }
+
+    /// Sets a custom theme
+    /// - Parameter theme: A theme that will be applied to the Authenticator and all its views
+    public func authenticatorTheme(_ theme: AuthenticatorTheme) -> some View {
+        environment(\.authenticatorTheme, theme)
+    }
+
+    func authenticationService(_ authenticationService: AuthenticationService) -> some View {
+        environment(\.authenticationService, authenticationService)
+    }
+
+    @ViewBuilder private func createView(for step: Step) -> some View {
+        switch step {
+        case .loading:
+            ProgressView()
+                .frame(minWidth: 60, maxWidth: .infinity, minHeight: 60, maxHeight: .infinity)
+        case .signIn:
+            signInContent
+        case .confirmSignInWithNewPassword:
+            confirmSignInContentWithNewPasswordContent
+        case .confirmSignInWithMFACode:
+            confirmSignInContentWithMFACodeContent
+        case .confirmSignInWithCustomChallenge:
+            confirmSignInContentWithCustomChallengeContent
+        case .resetPassword:
+            resetPasswordContent
+        case .confirmResetPassword:
+            confirmResetPasswordContent
+        case .signUp:
+            signUpContent
+        case .confirmSignUp:
+            confirmSignUpContent
+        case .verifyUser:
+            verifyUserContent
+        case .confirmVerifyUser:
+            confirmVerifyUserContent
+        case .error(let error):
+            errorContentBuilder(error)
+        case .signedIn(_):
+            // Should never happen
+            EmptyView()
+        }
+    }
+
+    private var contentTransition: AnyTransition {
+        if previousStep == .loading {
+            return .opacity
+        }
+
+        return viewModifiers.contentTransition
+    }
+
+    private func setUpContentStates(_ contentStates: NSHashTable<AuthenticatorBaseState>) {
+        for contentState in contentStates.allObjects {
+            contentState.configure(with: state)
+        }
+    }
+}
+
+extension Authenticator {
+    private struct ViewModifiers {
+        var hidesSignUpButton = false
+        var contentAnimation: Animation = .easeInOut(duration: 0.25)
+        var contentTransition: AnyTransition = .opacity
+        var signUpFields: [SignUpField] = []
+    }
+}
---
+++ Sources/Configuration/AmplifyConfiguration.swift
@@ -0,0 +1,180 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import Amplify
+@_spi(InternalAmplifyConfiguration) import AWSCognitoAuthPlugin
+import Foundation
+
+/// Represents an error with the Amplify configuration
+public struct AmplifyConfigurationError: Error, Equatable {
+    private let name: String
+    private init(name: String) {
+        self.name = name
+    }
+
+    /// The Amplify Auth's plugin could not be retrieved
+    public static let missingPlugin = AmplifyConfigurationError(name: "missingPlugin")
+
+    /// The Amplify Auth's configuration is missing
+    public static let missingConfiguration = AmplifyConfigurationError(name: "missingConfiguration")
+
+    /// The Amplify Auth configuration exists but it has unexpected values
+    public static let incorrectConfiguration = AmplifyConfigurationError(name: "incorrectConfiguration")
+}
+
+struct AmplifyConfiguration {
+    var cognito: CognitoConfiguration
+
+    init() throws {
+        guard let plugin = try? Amplify.Auth.getPlugin(for: "awsCognitoAuthPlugin") as? AWSCognitoAuthPlugin else {
+            Self.log.error("Unable to retrieve the AWSCognitoAuthPlugin")
+            throw AmplifyConfigurationError.missingPlugin
+        }
+
+        guard let configuration = plugin.jsonConfiguration,
+              let cognitoConfiguration = configuration.value(at: "Auth.Default") else {
+            Self.log.error("Unable to read Auth.Default from the configuration")
+            throw AmplifyConfigurationError.missingConfiguration
+        }
+
+        guard case .object(let passwordSettings) = cognitoConfiguration["passwordProtectionSettings"] else {
+            Self.log.error("passwordProtectionSettings is missing")
+            throw AmplifyConfigurationError.incorrectConfiguration
+        }
+
+        var minLength: Int
+        if case .number(let value) = passwordSettings["passwordPolicyMinLength"] {
+            minLength = Int(value)
+        } else if case .string(let value) = passwordSettings["passwordPolicyMinLength"],
+                  let intValue = Int(value) {
+            minLength = intValue
+        } else {
+            Self.log.error("passwordPolicyMinLength is missing")
+            throw AmplifyConfigurationError.incorrectConfiguration
+        }
+
+        var characterPolicy: [CognitoConfiguration.PasswordCharacterPolicy] = []
+        if case .array(let characters) = passwordSettings["passwordPolicyCharacters"] {
+            characterPolicy = characters.compactMap { value in
+                guard case .string(let string) = value else {
+                    return nil
+                }
+
+                return .init(rawValue: string)
+            }
+        }
+
+        let passwordProtectionSettings = CognitoConfiguration.PasswordProtectionSettings(
+            minLength: minLength,
+            characterPolicy: characterPolicy
+        )
+
+        var usernameAttributes: [CognitoConfiguration.UsernameAttribute] = []
+        if case .array(let attributes) = cognitoConfiguration["usernameAttributes"] {
+            usernameAttributes = attributes.compactMap { value in
+                guard case .string(let string) = value else {
+                    return nil
+                }
+
+                return .init(rawValue: string)
+            }
+        }
+
+        var signUpAttributes: [CognitoConfiguration.SignUpAttribute] = []
+        if case .array(let attributes) = cognitoConfiguration["signupAttributes"] {
+            signUpAttributes = attributes.compactMap { value in
+                guard case .string(let string) = value else {
+                    return nil
+                }
+
+                return .init(rawValue: string)
+            }
+        }
+
+        var verificationMechanisms: [CognitoConfiguration.VerificationMechanism] = []
+        if case .array(let attributes) = cognitoConfiguration["verificationMechanisms"] {
+            verificationMechanisms = attributes.compactMap { value in
+                guard case .string(let string) = value else {
+                    return nil
+                }
+
+                return .init(rawValue: string)
+            }
+        }
+
+        self.cognito = CognitoConfiguration(
+            usernameAttributes: usernameAttributes,
+            signupAttributes: signUpAttributes,
+            passwordProtectionSettings: passwordProtectionSettings,
+            verificationMechanisms: verificationMechanisms
+        )
+    }
+}
+
+struct CognitoConfiguration {
+    enum UsernameAttribute: String, Decodable {
+        case username = "USERNAME"
+        case email = "EMAIL"
+        case phoneNumber = "PHONE_NUMBER"
+    }
+
+    enum SignUpAttribute: String, Decodable {
+        case address = "ADDRESS"
+        case birthDate = "BIRTHDATE"
+        case email = "EMAIL"
+        case familyName = "FAMILY_NAME"
+        case gender = "GENDER"
+        case givenName = "GIVEN_NAME"
+        case middleName = "MIDDLE_NAME"
+        case name = "NAME"
+        case nickname = "NICKNAME"
+        case phoneNumber = "PHONE_NUMBER"
+        case preferredUsername = "PREFERRED_USERNAME"
+        case profile = "PROFILE"
+        case website = "WEBSITE"
+    }
+
+    enum VerificationMechanism: String, Decodable {
+        case email = "EMAIL"
+        case phoneNumber = "PHONE_NUMBER"
+    }
+
+    enum PasswordCharacterPolicy: String, Decodable {
+        case lowercase = "REQUIRES_LOWERCASE"
+        case uppercase = "REQUIRES_UPPERCASE"
+        case numbers = "REQUIRES_NUMBERS"
+        case symbols = "REQUIRES_SYMBOLS"
+    }
+
+    struct PasswordProtectionSettings: Decodable {
+        var minLength: Int
+        var characterPolicy: [PasswordCharacterPolicy]
+    }
+
+    var usernameAttributes: [UsernameAttribute]
+    var signupAttributes: [SignUpAttribute]
+    var passwordProtectionSettings: PasswordProtectionSettings
+    var verificationMechanisms: [VerificationMechanism]
+
+    var usernameAttribute: UsernameAttribute {
+        if let usernameAttribute = usernameAttributes.first {
+            return usernameAttribute
+        }
+
+        return .username
+    }
+
+    static var empty: CognitoConfiguration {
+        .init(
+            usernameAttributes: [],
+            signupAttributes: [],
+            passwordProtectionSettings: .init(minLength: 0, characterPolicy: []),
+            verificationMechanisms: [])
+    }
+}
+
+extension AmplifyConfiguration: AuthenticatorLogging {}
