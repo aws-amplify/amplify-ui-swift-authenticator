152d054dcd7c7560adb289064fe8538b580ceea7
Sebastian Villena <97059974+ruisebas@users.noreply.github.com>
2023-04-14 14:02:40 -0400

    feat(Authenticator): Adding primitives views (#3)
    * feat(Authenticator): Adding primitives views
    
    * Adding some documentation to each primitive

---
+++ Sources/Authenticator/Views/Primitives/PasswordField.swift
@@ -0,0 +1,116 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import SwiftUI
+
+/// This field encapsulates a native TextField and a SecureField in a single component,
+/// providing a button to toggle betweem them, as is common.
+/// It also applies Amplify UI's theming
+struct PasswordField: View {
+    @Environment(\.authenticatorTheme) var theme
+    @ObservedObject private var validator: Validator
+    @Binding private var text: String
+    @FocusState private var focusedField: FieldType?
+    @State private var isShowingPassword: Bool = false
+    private let label: String?
+    private let placeholder: String
+
+    init(_ label: String,
+         text: Binding<String>,
+         placeholder: String,
+         validator: Validator? = nil) {
+        self.label = label
+        self._text = text
+        self.placeholder = placeholder
+        self.validator = validator ?? .init(
+            using: FieldValidators.none
+        )
+        self.validator.value = text
+    }
+
+    init(_ placeholder: String,
+         text: Binding<String>,
+         validator: Validator? = nil) {
+        self.label = nil
+        self._text = text
+        self.placeholder = placeholder
+        self.validator = validator ?? .init(
+            using: FieldValidators.none
+        )
+        self.validator.value = text
+    }
+
+    var body: some View {
+        AuthenticatorField(
+            label,
+            placeholder: placeholder,
+            validator: validator,
+            isFocused: isFocused
+        ) {
+            HStack {
+                createInput()
+                    .disableAutocorrection(true)
+                    .onChange(of: text) { text in
+                        if validator.state != .normal || !text.isEmpty {
+                            validator.validate()
+                        }
+                    }
+                    .onChange(of: focusedField) { focused in
+                        if focused == nil {
+                            validator.validate()
+                        }
+                    }
+                #if os(iOS)
+                    .autocapitalization(.none)
+                    .frame(height: 25)
+                    .padding([.top, .bottom, .leading], theme.Fields.style.padding)
+                #endif
+                if focusedField != nil, !text.isEmpty {
+                    ImageButton(showPasswordImage) {
+                        isShowingPassword.toggle()
+                        focusedField = isShowingPassword ? .plain : .secure
+                    }
+                    .tintColor(showPasswordButtonColor)
+                    .padding([.top, .bottom, .trailing], theme.Fields.style.padding)
+                }
+            }
+            .animation(.linear(duration: 0.1), value: isShowingPassword)
+        }
+    }
+
+    @ViewBuilder private func createInput() -> some View {
+        if isShowingPassword {
+            SwiftUI.TextField(placeholder, text: $text)
+                .focused($focusedField, equals: .plain)
+        } else {
+            SwiftUI.SecureField(placeholder, text: $text)
+                .focused($focusedField, equals: .secure)
+        }
+    }
+
+    private var isFocused: Bool {
+        return focusedField != nil
+    }
+
+    private var showPasswordButtonColor: Color {
+        switch validator.state {
+        case .normal:
+            return theme.Colors.Border.interactive
+        case .error:
+            return theme.Colors.Border.error
+        }
+    }
+
+    private var showPasswordImage: ImageButton.Image {
+        return isShowingPassword ? .showPassword : .hidePassword
+    }
+
+    private enum FieldType: Hashable {
+        case plain
+        case secure
+    }
+}
---
+++ Sources/Authenticator/Views/Primitives/RadioButton.swift
@@ -0,0 +1,58 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+import SwiftUI
+
+/// This represents a simple Button with a Radio Button-like look.
+/// It automatically toggles its state on tap
+struct RadioButton: View {
+    @Environment(\.authenticatorTheme) var theme
+    @Binding private var isSelected: Bool
+    private let label: String
+    private let action: () -> ()
+
+    init(
+        label: String,
+        isSelected: Binding<Bool>,
+        action: @escaping () -> ()
+    ) {
+        self.label = label
+        self._isSelected = isSelected
+        self.action = action
+    }
+
+    var body: some View {
+        SwiftUI.Button(
+            action: {
+                isSelected.toggle()
+                action()
+            },
+            label: {
+                HStack(alignment: .center) {
+                    SwiftUI.Image(
+                        systemName: isSelected
+                        ? "circle.inset.filled"
+                        : "circle"
+                    )
+                    .font(.system(size: 24))
+                    .foregroundColor(foregroundColor)
+                    Text(label)
+                        .foregroundColor(theme.Colors.Foreground.primary)
+                    Spacer()
+                }
+            }
+        )
+    }
+
+    private var foregroundColor: Color {
+        if isSelected {
+            return theme.Colors.Background.interactive
+        }
+
+        return theme.Colors.Border.primary
+    }
+}
