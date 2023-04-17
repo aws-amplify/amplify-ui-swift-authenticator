12553c906f25570bc92e3b34754c78669d007aac
Sebastian Villena <97059974+ruisebas@users.noreply.github.com>
2023-04-17 11:22:33 -0400

    chore: Updating Amplify dependency version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["Authenticator"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/aws-amplify/amplify-swift", from: "2.8.0")
+        .package(url: "https://github.com/aws-amplify/amplify-swift", from: "2.8.1"),
     ],
     targets: [
         .target(
