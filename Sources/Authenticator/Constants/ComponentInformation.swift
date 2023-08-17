e6cca20a23b4e6d1834a8df35273328ada6a80a9
Sebastian Villena <97059974+ruisebas@users.noreply.github.com>
2023-08-17 15:28:09 -0400

    fix(DatePicker): Fixing the format used to parse the date into a string (#31)

--- Sources/Authenticator/Constants/ComponentInformation.swift
+++ Sources/Authenticator/Constants/ComponentInformation.swift
@@ -8,6 +8,6 @@
 import Foundation
 
 public class ComponentInformation {
-    public static let version = "1.0.2"
+    public static let version = "1.0.3"
     public static let name = "amplify-ui-swift-authenticator"
 }
