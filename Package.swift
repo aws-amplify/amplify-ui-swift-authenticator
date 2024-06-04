35004b2b0d1948bb31dc6146a37c3d2a82b0d80a
Sebastian Villena <97059974+ruisebas@users.noreply.github.com>
2024-06-04 12:19:00 -0400

    chore: Preparing release and pinning Amplify version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["Authenticator"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/aws-amplify/amplify-swift", from: "2.16.0"),
+        .package(url: "https://github.com/aws-amplify/amplify-swift", "2.16.0"..<"2.35.0"),
     ],
     targets: [
         .target(
