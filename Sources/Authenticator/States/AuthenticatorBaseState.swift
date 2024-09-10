9faafb504a39eb0ba657bed52a70200815aaca5c
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2024-09-09 21:32:19 -0400

    add support for email mfa code

--- Sources/Authenticator/States/AuthenticatorBaseState.swift
+++ Sources/Authenticator/States/AuthenticatorBaseState.swift
@@ -67,7 +67,8 @@ public class AuthenticatorBaseState: ObservableObject {
     func nextStep(for result: AuthSignInResult) async throws -> Step {
         log.verbose("Sign In next step is \(result.nextStep)")
         switch result.nextStep {
-        case .confirmSignInWithSMSMFACode(let details, _):
+        case .confirmSignInWithSMSMFACode(let details, _),
+                .confirmSignInWithEmailMFACode(let details):
             return .confirmSignInWithMFACode(deliveryDetails: details)
         case .confirmSignInWithCustomChallenge(_):
             return .confirmSignInWithCustomChallenge
