b855272b298288582d28e89bed82f8f781508390
Harshdeep Singh <6162866+harsh62@users.noreply.github.com>
2024-09-27 11:29:08 -0400

    pushing a test.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["Authenticator"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/aws-amplify/amplify-swift", from: "2.35.0"),
+        .package(url: "https://github.com/aws-amplify/amplify-swift", branch: "feat/email-mfa-support-public"),
     ],
     targets: [
         .target(
