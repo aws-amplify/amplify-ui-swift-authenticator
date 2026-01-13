cea0325fa38988b6105e5fb59a9cc472de199413
Matt Creaser <mattwcc@amazon.com>
2026-01-13 13:35:43 -0400

    Update amplify-swift to 2.53.2

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["Authenticator"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/aws-amplify/amplify-swift", from: "2.45.0")
+        .package(url: "https://github.com/aws-amplify/amplify-swift", from: "2.53.2")
     ],
     targets: [
         .target(
